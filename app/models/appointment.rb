d31f43f7b85ebffbbac0de5036213d1c0023ce12
Ben Merkel <ben@merkelfamily.com>
2014-11-17 14:49:01 -0700

    Added some validation to the appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,14 @@
 class Appointment < ActiveRecord::Base
+  validates :parent_names, :email_addresses, presence: true
+  # validates_presence_of :parent_names, :phone_numbers, :email_addresses,
+  #                       :children_names, :children_ages
+  validate :tour_must_be_future
+
+  def tour_must_be_future
+    required_days_in_future = 7
+    if tour_date < Time.now + required_days_in_future * (24 * 60 * 60)
+      errors.add(:tour_date, "must be at least #{required_days_in_future} day in the future")
+    end
+  end
 end
+
